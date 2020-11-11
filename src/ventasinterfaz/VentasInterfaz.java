c9f818371acd271dfddb1be629215d86383f47a6
Rasec <rasec.camacho19@gmail.com>
2020-11-11 16:43:32 -0700

    Primero commit

---
+++ src/ventasinterfaz/VentasInterfaz.java
@@ -0,0 +1,11 @@
+package ventasinterfaz;
+
+public class VentasInterfaz {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventas objVentas = new ventas();
+        objVentas.setVisible(true);
+    }
+
+}
